2b427263805761b9aa4d7e07ed26b6686752e2de
meme_papa <153528643+DeadpooHackes@users.noreply.github.com>
2025-07-14 04:58:59 +0530

    Update ForensicReports_UI.py

--- ForensicReports_UI.py
+++ ForensicReports_UI.py
@@ -9,8 +9,8 @@ from PIL import Image, ImageTk
 import sys
 
 # Telegram Bot Setup
-TELEGRAM_BOT_TOKEN = "ENTER TOKEN"
-TELEGRAM_CHAT_ID = "ENTER CHAT ID"
+TELEGRAM_BOT_TOKEN = "YOUR_BOT_TOKEN"
+TELEGRAM_CHAT_ID = "YOUR_CHAT_ID"
 bot = telebot.TeleBot(TELEGRAM_BOT_TOKEN)
 
 # Output Directory
